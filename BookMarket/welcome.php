e2adb62ac7d375623aece529f9c1fa213e507fcc
jooho2075 <parkjh2075@icloud.com>
2024-09-03 21:59:02 +0900

    first commit

---
+++ BookMarket/welcome.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome</title>
+</head>
+<body>
+    <!--week1-->
+    <h1>도서 쇼핑몰에 오신 것을 환영합니다!</h1>
+    <h3>Welcome to Web Market!</h3>
+</body>
+</html>
---
+++ week1.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My PHP Site</title>
+</head>
+<body>
+    <!--php section-->
+    <?php
+        echo "Hello My PHP<br><br>";
+        echo "My Second Semester<br>";
+        echo "<img src='http://www.loremflickr.com/320/240'>";
+    ?>
+    <!--echo_문자작성-->
+    
+</body>
+</html>
