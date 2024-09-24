58c22b33cea9d8389f336acc712cbb9a10afbe35
jooho2075 <parkjh2075@icloud.com>
2024-09-25 08:34:36 +0900

    24.09.25_Week4_fix_version2

--- BookMarket/books.php
+++ BookMarket/books.php
@@ -36,16 +36,15 @@
     <script src="https://getbootstrap.com/docs/5.3/dist/js/bootstrap.bundle.min.js" ></script>   
 </head>
 <body class="d-flex flex-column h-100">
-   
 
 <header>
-  <!-- Fixed navbar -->
+<!-- Fixed navbar -->
     <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
         <div class="container-fluid">
             <a class="navbar-brand" href="./welcome.php">
                 <!--            
                 <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor" class="bi bi-book" viewBox="0 0 16 16">
-                    <path d="M1 2.828c.885-.37 2.154-.769 3.388-.893 1.33-.134 2.458.063 3.112.752v9.746c-.935-.53-2.12-.603-3.213-.493-1.18.12-2.37.461-3.287.811V2.828zm7.5-.141c.654-.689 1.782-.886 3.112-.752 1.234.124 2.503.523 3.388.893v9.923c-.918-.35-2.107-.692-3.287-.81-1.094-.111-2.278-.039-3.213.492V2.687zM8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783z"/>
+                <path d="M1 2.828c.885-.37 2.154-.769 3.388-.893 1.33-.134 2.458.063 3.112.752v9.746c-.935-.53-2.12-.603-3.213-.493-1.18.12-2.37.461-3.287.811V2.828zm7.5-.141c.654-.689 1.782-.886 3.112-.752 1.234.124 2.503.523 3.388.893v9.923c-.918-.35-2.107-.692-3.287-.81-1.094-.111-2.278-.039-3.213.492V2.687zM8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783z"/>
                 </svg>
                 -->
                 <i class="fa-solid fa-book-open"></i>
@@ -70,26 +69,25 @@
 </header>
 
 <?php
-     $greeting = "도서목록";
-     $tagline = "Welcome to Web Market!";
+    $greeting = "도서목록";
+    $tagline = "Welcome to Web Market!";
 ?>  
 <br>
 
- <main > 
+<main >
     <div class="container py-5">
-        
         <div class="p-5 mb-4 bg-body-tertiary rounded-3">
             <div class="container-fluid py-5">
                 <h1 class="display-5 fw-bold"><?php echo $greeting ?></h1>
                 <p class="col-md-8 fs-4">BookList</p>
             </div>
         </div> 
-        
+
         <div class="row align-items-md-stretch text-center">
-          <?php
-          for($i = 0; $i < 3; $i++) {
-            $id = "ISBN" . (1234+$i);
-          ?>
+            <?php
+                for($i = 0; $i < 3; $i++) {
+                    $id = "ISBN" . (1234+$i);
+            ?>
             <div class="col-md-4">
                 <div class="h-100 p-5">
                     <h2><?php echo $BookArray[$id]["name"]; ?></h2>
@@ -98,24 +96,24 @@
                     <p><?= $BookArray[$id]["unitPrice"]; ?>원</p>
                 </div>
             </div>
-          <?php
-            }
-          ?>
+                <?php
+                }
+                ?>
         </div>
         <!--
         <div class="row">
-            <div class="col-6" style="border:1px solid red">Col-1</div>
-            <div class="col-3" style="border:1px solid red">Col-2</div>
-            <div class="col-3" style="border:1px solid red">Col-3</div>
+        <div class="col-6" style="border:1px solid red">Col-1</div>
+        <div class="col-3" style="border:1px solid red">Col-2</div>
+        <div class="col-3" style="border:1px solid red">Col-3</div>
         </div>
-    </div>
+        </div>
         -->
 </main>
 
-<footer class="footer mt-auto py-3 bg-body-tertiary">
-    <div class="container">
-        <span class="text-body-primary"> &copy; BookMarket</span>
-    </div>
-</footer>
+    <footer class="footer mt-auto py-3 bg-body-tertiary">
+        <div class="container">
+            <span class="text-body-primary"> &copy; BookMarket</span>
+        </div>
+    </footer>
 </body>
 </html>
