4d24addf75942637bb4e5148d7495559430476d8
jooho2075 <parkjh2075@icloud.com>
2024-11-26 11:09:45 +0900

    24.11.26_Week13_MYSQL practice2

--- week13_dbconn.php
+++ week13_dbconn.php
@@ -7,5 +7,27 @@
         die("DB 연결 실패 : ". mysqli_connect_error());
     }
 
-    echo "DB 연결 성공 " .mysqli_get_host_info($conn);
+    echo "DB 연결 성공" .mysqli_get_host_info($conn)."<br>";
+
+    // $sql = "insert into student(name, password) values('ho', '3456')";
+    // $sql = "update student set name='hohohohoho' where id=4";
+    // $sql = "delete from student where id=4";
+    // mysqli_query($conn, $sql);
+
+    $sql = "select * from student";
+    $result = mysqli_query($conn, $sql);
+
+    echo "레코드 수 : ";
+    echo mysqli_num_rows($result)."<br>"; // 결과의 갯수를 읽어줌
+
+    while($row = mysqli_fetch_array($result)) {
+        // echo $row['id']; -> 단독 변수인 경우 인덱스를 문자열로 반드시 표시
+        echo "ID : $row[id] | "; // 문자열 내부에 사용될 경우 인덱스는 그대로 따옴표 없이 사용 
+        echo "NAME : $row[name] | ";
+        echo "PASS : $row[password]<br>";
+    }
+
+    mysqli_free_result($result);
+
+    mysqli_close($conn);
 ?>
